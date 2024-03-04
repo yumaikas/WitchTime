2085a49557bccf6aef5a8c9a22db7f46accc5ac4
Andrew Owen <yumaikas94@gmail.com>
2024-03-04 09:06:24 -0700

    Start work on map loader

---
+++ Map/GameMap.gd
@@ -0,0 +1,61 @@
+class_name GameMap extends Reference
+
+signal move_room(from, to)
+
+var rooms = {}
+var currentRoom
+
+var settings
+func _init():
+    pass
+
+func location_of(mapCoord: Vector2):
+    var rs = settings.room_size
+    var room_coord = Vector2(mapCoord.x / rs.x, mapCoord.y / rs.y)
+    var in_room_coord = Vector2(mapCoord.x % rs.x, mapCoord.y % rs.y)
+
+    return {"map": room_coord, "room": in_room_coord}
+
+func load_map(bgTiles: TileMap, solids: TileMap, settings: GameMap.GameMapSettings):
+    self.settings = settings
+    var loading_rooms = {}
+
+    for cell in bgTiles.get_used_cells():
+        var coords = location_of(cell)
+        var id = solids.get_cellv(cell)
+
+        if not loading_rooms.has(coords.map):
+            loading_rooms[coords.map] = MapRoom.new(coords.map)
+        loading_rooms[coords.map].tiles[coords.room] = id
+
+
+    var tileset: TileSet = solids.tile_set
+    for cell in solids.get_used_cells():
+        var coords = location_of(cell)
+        var id = solids.get_cellv(cell)
+
+        if not loading_rooms.has(coords.map):
+            loading_rooms[coords.map] = MapRoom.new(coords.map)
+        loading_rooms[coords.map].solids[coords.room] = tileset.tile_get_name(id)
+
+    pass
+
+
+
+func can_move(to):
+    return true
+
+func setupBaseMap():
+    var startRoom = MapRoom.new(Vector2(4,8))
+    var firstPuzzleRoom = MapRoom.new(Vector2(4,7))
+    currentRoom = startRoom
+
+class GameMapSettings extends Reference:
+
+    var room_size
+
+    func _init():
+        pass
+    
+    static func from_json(json: String):
+        pass
--- Steppers/GameCore.gd
+++ Steppers/GameCore.gd
@@ -3,15 +3,15 @@ class_name GameCore extends Node
 signal event(data)
 
 var domains = {
-    "witch": { "enemies": [], "gadgets": [], },
-    "gemini": { "enemies": [], "gadgets": [], },
-    "viridian": { "enemies": [], "gadgets": [], },
-    "ordok": { "enemies": [], "gadgets": [], },
-    "atlas": { "enemies": [], "gadgets": [], },
+	"witch": { "enemies": [], "gadgets": [], },
+	"gemini": { "enemies": [], "gadgets": [], },
+	"viridian": { "enemies": [], "gadgets": [], },
+	"ordok": { "enemies": [], "gadgets": [], },
+	"atlas": { "enemies": [], "gadgets": [], },
 }
 
 var witch = {
-    "pos": Vector2(0,0)
+	"pos": Vector2(0,0)
 }
 
 var _forth = """
@@ -21,20 +21,20 @@ var _forth = """
 :gadgets-in-domain [ .domains domain get :gadgets get ] def
 
 :tick-domain [ +scope =domain
-    enemies-in-domain [ &tick() ] each
-    gadgets-in-domain [ &tick() ] each
-    -scope
+	enemies-in-domain [ &tick() ] each
+	gadgets-in-domain [ &tick() ] each
+	-scope
 ] def
 
 :witch-pos [ .witch :pos get ] def
 
 :witch-move [ +scope =dir
-    self &can_witch_move( dir ) 
-        [ self &move_witch( dir ) ~done ] 
-        [ self &nomove_witch() ] 
-    if-else
-    :witch tick-domain
-    -scope 
+	self &can_witch_move( dir ) 
+		[ self &move_witch( dir ) ~done ] 
+		[ self &nomove_witch() ] 
+	if-else
+	:witch tick-domain
+	-scope 
 ] def
 
 :turn [ self ~event exec ] def
@@ -46,61 +46,45 @@ var _forth = """
 var vm
 
 func do(name, data):
-    # Want name to be TOS, data after
-    emit_signal("event", data, name)
+	# Want name to be TOS, data after
+	emit_signal("event", data, name)
 
 func _ready():
-    var map = Map.new()
-    map.setupBaseMap()
-    vm = GDForth.new(_forth, self)
+	var map = GameMap.new()
+	map.setupBaseMap()
+	vm = GDForth.new(_forth, self)
 
 func can_witch_move(_dir): 
-    return true
+	return true
 
 func move_witch(dir):
-    witch.pos += dir
-    return GDForth.nowait_of("done", null)
-
-class Map extends Reference:
-    var rooms = {}
-    var currentRoom
-
-    func _init():
-        pass
-
-    func can_move(to):
-        return true
-
-
-    func setupBaseMap():
-        var startRoom = MapRoom.new(Vector2(4,8))
-        var firstPuzzleRoom = MapRoom.new(Vector2(4,7))
-        currentRoom = startRoom
+	witch.pos += dir
+	return GDForth.nowait_of("done", null)
 
 class Door extends Reference:
-    var endA: Location
-    var endB: Location
+	var endA: Location
+	var endB: Location
 
-    func init(one, A):
-        endA = one
-        endB = A
-    
-    func get_other_end(end):
-        if end == endA:
-            return endB
-        elif end == endB:
-            return endA
-        else:
-            return null
+	func init(one, A):
+		endA = one
+		endB = A
+	
+	func get_other_end(end):
+		if end == endA:
+			return endB
+		elif end == endB:
+			return endA
+		else:
+			return null
 
-    
+	
 class Location extends Reference:
-    var coord
-    var room
+	var coord
+	var room
 
-    func _init(x,y, room):
-        coord = Vector2(x,y)
-        self.room = room
+	func _init(x,y, room):
+		coord = Vector2(x,y)
+		self.room = room
 
 
 
